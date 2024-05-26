3be211859766f2734910affe697dc71e7f81b3b9
Krešimir Borbaš <kresimir.borbas@gmail.com>
2024-05-26 20:19:41 +0200

    update README.MD

---
+++ assets/deploy_azure_web_app.ps1
@@ -0,0 +1,6 @@
+﻿$location = "Germany West Central"
+$webAppName = "app-azuredemo-staging-4"
+$appServicePlanName = "asp-azuredemo-staging"
+$resourceGroupName = "rg-azuredemo-staging"
+
+New-AzWebApp -Name $webAppName -Location $location -AppServicePlan $appServicePlanName -ResourceGroupName $resourceGroupName
---
+++ assets/deploy_azure_web_app_alternate.ps1
@@ -0,0 +1,6 @@
+﻿$location = Read-Host -Prompt "Enter the Location (e.g., Germany West Central)"
+$webAppName = Read-Host -Prompt "Enter the Web App name"
+$appServicePlanName = Read-Host -Prompt "Enter the App Service Plan name"
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+
+New-AzWebApp -Name $webAppName -Location $location -AppServicePlan $appServicePlanName -ResourceGroupName $resourceGroupName
